b68b02e4bf5c1c1ad76741807907228c9ef4402b
Keith Bauer <onesadcookie@gmail.com>
2022-05-23 11:08:23 +1200

    add absurdle-best-guess, for what little it's worth

---
+++ README.md
@@ -0,0 +1,16 @@
+# Fun with Wordle
+
+* [Wordle](https://www.nytimes.com/games/wordle/index.html)
+* [Absurdle](https://qntm.org/files/absurdle/absurdle.html)
+
+## lib `wordle`
+
+Library for working with Wordle puzzles.
+
+## bin `wordle-spoilers`
+
+Which patterns of info tell you most about the words? In other words, how much of a spoiler is the "share results" button?
+
+## bin `absurdle-best-guess`
+
+Trying to figure out how to get the lowest score at absurdle. I think this one's fundamentally flawed.
---
+++ absurdle-best-guess/src/main.rs
@@ -0,0 +1,27 @@
+use wordle::{Score, POSSIBLE_WORDS};
+
+fn pass(lexicon: &[&'static str]) -> (&'static str, Vec<&'static str>) {
+    let mut bests = std::collections::HashMap::<&'static str, Vec<&'static str>>::new();
+    for word in lexicon.iter() {
+        let mut result = std::collections::HashMap::<Score, Vec<&'static str>>::new();
+        for guess in lexicon.iter() {
+            let s = wordle::score(word, guess);
+            result.entry(s).or_insert(vec![]).push(guess);
+        }
+        let best = result.iter().max_by_key(|(_, v)| v.len()).unwrap();
+        println!("best for {} is {:?} ({})", word, best.0, best.1.len());
+        bests.insert(word, best.1.clone());
+    }
+    let result = bests.iter().min_by_key(|(_, v)| v.len()).unwrap();
+    println!("best overall: {} ({})", result.0, result.1.len());
+    (result.0, result.1.clone())
+}
+
+fn main() {
+    let mut guess = pass(&POSSIBLE_WORDS);
+    while guess.1.len() > 1 {
+        println!("{}: {}", guess.0, guess.1.len());
+        guess = pass(&guess.1);
+    }
+    println!("{}", guess.0);
+}
